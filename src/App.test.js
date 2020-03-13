2da71ef34a1f1ce299d9331e2b15f45651293abe
Javier Umana <javierumanafs@gmail.com>
2020-03-12 23:50:40 -0400

    aaa

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,13 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from './components/ContactForm';
 
-test("renders App without crashing", () => {
-  render(<App />);
+
+test('contact form taking user inputs', () => {
+  const { getByLabelText } = render(<ContactForm />);
+
+  const firstInput = getByLabelText(firstInput, { target: { name: 'firstName', value: 'Ty' } })
+
+  fireEvent.change(firstInput, { target: { name: 'firstName', value: 'Ty' } })
 });
